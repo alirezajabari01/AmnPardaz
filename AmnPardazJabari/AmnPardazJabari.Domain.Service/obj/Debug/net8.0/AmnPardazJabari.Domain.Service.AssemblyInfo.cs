e63dbe9fa4455facaafe64080411f6d53c1573d3
alirezajabari01 <94402734+alirezajabari01@users.noreply.github.com>
2024-09-20 17:29:22 +0330

    dockerized

--- AmnPardazJabari/AmnPardazJabari.Domain.Service/obj/Debug/net8.0/AmnPardazJabari.Domain.Service.AssemblyInfo.cs
+++ AmnPardazJabari/AmnPardazJabari.Domain.Service/obj/Debug/net8.0/AmnPardazJabari.Domain.Service.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AmnPardazJabari.Domain.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9e4a0da702a02c5b47cacd68c4633c63bab6a63")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0d81809fc90cd3c9ccefc6b3edaaf2e1d6c2ef8")]
 [assembly: System.Reflection.AssemblyProductAttribute("AmnPardazJabari.Domain.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AmnPardazJabari.Domain.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
